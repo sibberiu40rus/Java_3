6fb264eb7785578fd54925c54a39fa3549510ad7
siberiu40rus <siberiu40rus@gmail.com>
2019-02-03 00:00:10 +0300

    done

---
+++ Java_3/src/Home_work_1.java
@@ -0,0 +1,37 @@
+public class Home_work_1 {
+    public static void main(String[] args) {
+        String[] words = {"a","b","c","d","e","f"};
+        toString(words);
+        changeArr2(words, 1,5);
+        System.out.println();
+        toString(words);
+
+    }
+
+    //поменяет местами каждые два элемента
+
+    public static void changeArr (String[] str) {
+        for (int i = 0; i < str.length; i++) {
+            String temp = str[i];
+            str[i] = str[++i];
+            str[i] = temp;
+        }
+    }
+
+    //поменяет местами элементы индексы которых переданны в параметрах метода
+
+    public static void changeArr2 (String[] str, int one, int two) {
+            String temp1 = str[one];
+            str[one] = str[two];
+            str[two] = temp1;
+
+    }
+
+    //отображаем массив
+
+    public static void toString (String[] str) {
+        for (String s : str) {
+            System.out.print(s + ", ");
+        }
+    }
+}
